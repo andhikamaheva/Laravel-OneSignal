670c7c3925adb64ebc74a841623cfb03f9d09174
andhikamaheva <andhikamaheva@gmail.com>
2016-04-16 06:02:02 +0700

    intial commit

---
+++ src/OneSignal/LaravelOneSignalProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Andhikaid\OneSignal;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelOneSignalProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
